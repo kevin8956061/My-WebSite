3bd8ddd57f478cdab8c31da1c14dc5b1877eb677
kevin8956061 <kevin8956061@gmail.com>
2016-09-12 16:06:38 +0800

    Add Find Me Controller

---
+++ ISay/Controllers/FindMeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISay.Controllers
+{
+    public class FindMeController : Controller
+    {
+        // GET: FindMe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
